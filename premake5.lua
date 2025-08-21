8ed6e0c146f1dee2ef9b0bdcde4ece6181b4bdff
aoaiin <1592885596@qq.com>
2025-08-21 22:24:55 +0800

    add premake file

---
+++ premake5.lua
@@ -0,0 +1,84 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	defines                     --??
+	{
+		"YAML_CPP_STATIC_DEFINE"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"     --??
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"     --??
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+--[[
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+    
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		
+		"include/**.h"
+	}
+    
+	includedirs
+	{
+		"include"
+	}
+    
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+    
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+    
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+]]
